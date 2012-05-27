739f2162ea37ccbe4e3d2dd211124a8ddd4fbc4d
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2012-05-27 17:59:46 +0300

    removed personal version of options.php

--- tests/options.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# If you want to perform Transport tests please be shure
-# that files located in directory tests/HTTP/_files are copied/symlinked
-# to the server, which is defined by following constant
-
-define('TEST_TRANSPORT_CURL_BASEURL', 'http://test.http.vognev.org.ua/');
